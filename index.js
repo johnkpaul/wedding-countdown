fd263712b805177873a242bb9500bb07fa9c9876
John K. Paul <john@johnkpaul.com>
2015-12-17 08:45:12 -0500

    Change to post wedding messages with emoji

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 var client = require('./twilio');
+var moment = require('moment');
+var ordinal = require('ordinal');
+var emoji = require('node-emoji');
 
 var users = require('./config/secrets.js').users;
-var daysUntilWedding = daysUntil(2015, 12, 4);
+var honeyMoons = [];
+var daysSinceCommunalWedding = -1 * daysUntil(2015, 12, 4);
+var daysSinceLegalWedding = -1 * daysUntil(2015, 12, 2);
+var daysInFirstTartSun = -1 * daysUntil(2015, 12, 13);
+
+var today = moment();
+
+var communal = today.diff(moment('2015-12-04'), 'days');
+var legal = today.diff(moment('2015-12-02'), 'days');
+var tartSun = today.diff(moment('2015-12-12'), 'days');
+
+var message = 'We\'ve been married communally for ' + communal + ' days, legally for ' + legal + ' days';
+
+message += '\nand it\'s the ' + ordinal(tartSun) + ' day of our first tartsun';
+message += '\n'+ emoji.get('kissing_heart');
 
 users.forEach(function(user){
   var to = user.phone_number;
 
-  client.send(to, daysUntilWedding + " days until our wedding!").then(function(message){
+  //console.log(message);
+  client.send(to, message).then(function(message){
     console.log(JSON.stringify(message));
   });
 
