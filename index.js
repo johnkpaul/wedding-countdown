fb7fead845ff1d457ac62d63ed90cd90bebba520
Skylar Panuska <skylar.panuska@gmail.com>
2016-06-03 19:00:59 -0400

    removes unused code, removes date variables into their own module

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-var client = require('./twilio');
-var moment = require('moment');
+var client =  require('./twilio');
+var moment =  require('moment');
 var ordinal = require('number-to-words').toOrdinal;
-var emoji = require('node-emoji');
+var emoji =   require('node-emoji');
+var dates =   require('./weddingDates');
+var users =   require('./config/secrets.js').users;
 
-var users = require('./config/secrets.js').users;
-var honeyMoons = [];
-var daysSinceCommunalWedding = -1 * daysUntil(2015, 12, 4);
-var daysSinceLegalWedding = -1 * daysUntil(2015, 12, 2);
-var daysInFirstTartSun = -1 * daysUntil(2015, 12, 13);
+function composeMessage(opts) {
+  var communallyMarriedFor = opts.communallyMarriedFor;
+  var legallyMarriedFor = opts.legallyMarriedFor;
+  var daysInCurrentTartSun = opts.daysInCurrentTartSun;
+  var tartSunNumber = opts.tartSunNumber;
 
-var today = moment();
-
-var communal = today.diff(moment('2015 12 04', 'YYYY MM DD' ), 'days');
-var legal = today.diff(moment('2015 12 02', 'YYYY MM DD' ), 'days');
-var numberOfTartSuns = 3;
-var currentTartSun = today.diff(moment('2016 4 26', 'YYYY MM DD' ), 'days');
-
-function composeMessage() {
-  var marriagesCounts = 'We\'ve been married communally for ' + communal + ' days, legally for ' + legal + ' days';
-  var tartSunCount = '\nand it\'s the ' + ordinal(currentTartSun) + ' day of our ' + ordinal(numberOfTartSuns) + ' tartsun';
+  var marriagesCounts = 'We\'ve been married communally for ' + communallyMarriedFor + ' days, legally for ' + legallyMarriedFor + ' days';
+  var tartSunCount = '\nand it\'s the ' + ordinal(daysInCurrentTartSun) + ' day of our ' + ordinal(tartSunNumber) + ' tartsun';
   var kissyFace = '\n'+ emoji.get('kissing_heart');
+
   return marriagesCounts + tartSunCount + kissyFace;
 }
 
+var messageOptionsObject = { 
+  communallyMarriedFor: dates.daysSinceCommunalWedding, 
+  legallyMarriedFor: dates.daysSinceLegalWedding, 
+  daysInCurrentTartSun: dates.daysInCurrentTartSun, 
+  tartSunNumber: dates.numberOfTartSuns
+};
+
 users.forEach(function(user){
   var to = user.phone_number;
-  var message = composeMessage();
+  var message = composeMessage(messageOptionsObject);
 
   //console.log(message);
   client.send(to, message).then(function(message){
@@ -33,13 +35,5 @@ users.forEach(function(user){
   });
 
 });
-
-function daysUntil(year, month, day) {
-  var now = new Date(),
-      dateEnd = new Date(year, month - 1, day), // months are zero-based
-      days = (dateEnd - now) / 1000/60/60/24;   // convert milliseconds to days
-
-  return Math.round(days);
-}
 
 module.exports = { composeMessage };
