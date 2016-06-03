6b6495d51e71289c9505ef0c38cb6d8434ddfd38
Skylar Panuska <skylar.panuska@gmail.com>
2016-06-03 19:02:23 -0400

    adds test for composing the message to be sent

---
+++ index.test.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect;
+var countdown = require('./index');
+var moment = require('moment');
+
+describe("countdown message", function() {
+  it("is a string", function() {
+    var message = countdown.composeMessage({
+      communallyMarriedFor: 4,
+      legallyMarriedFor: 6,
+      daysInCurrentTartSun: 10,
+      tartSunNumber: 3
+    });
+
+    expect(message).to.contain('communally for 4');
+    expect(message).to.contain('legally for 6');
+    expect(message).to.contain('10th day');
+    expect(message).to.contain('3rd tartsun');
+
+    var message2 = countdown.composeMessage({
+      communallyMarriedFor: 85,
+      legallyMarriedFor: 1000,
+      daysInCurrentTartSun: 23,
+      tartSunNumber: 96
+    });
+
+    expect(message2).to.contain('communally for 85');
+    expect(message2).to.contain('legally for 1000');
+    expect(message2).to.contain('23rd day');
+    expect(message2).to.contain('96th tartsun');
+  });
+});
